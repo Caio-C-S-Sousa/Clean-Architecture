6d9a28d00d2e80dfaa8c90ac7676c7d226257729
Caio Sousa <caiyo.projeto@gmail.com>
2023-09-05 08:37:41 -0300

    Include swagger extension;

--- CleanArchSolution/WebApi/Extensions/SwaggerConfig.cs
+++ CleanArchSolution/WebApi/Extensions/SwaggerConfig.cs
@@ -4,6 +4,22 @@ public static class SwaggerConfig
 {
     public static void AddSwagger(this IServiceCollection serviceCollection)
     {
-        serviceCollection.AddSwaggerGen();
+        serviceCollection.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+            {
+                Version = "v1",
+                Title = "Udemy Web API"
+            });
+        });
+    }
+
+    public static void UseSwaggerLayout(this WebApplication webApplication)
+    {
+        if (webApplication.Environment.IsDevelopment())
+        {
+            webApplication.UseSwagger();
+            webApplication.UseSwaggerUI();
+        }
     }
 }
--- CleanArchSolution/WebApi/Program.cs
+++ CleanArchSolution/WebApi/Program.cs
@@ -12,11 +12,7 @@ builder.Services.AddSwagger();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwaggerLayout();
 
 app.UseHttpsRedirection();
 
