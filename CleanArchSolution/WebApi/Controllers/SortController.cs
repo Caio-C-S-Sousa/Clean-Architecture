6c5504ada6e88893af73b87242683280bb923d81
Caio Sousa <caiyo.projeto@gmail.com>
2023-10-02 11:27:11 -0300

    sort Controller class

---
+++ CleanArchSolution/WebApi/Controllers/SortController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SortController
+{
+    public SortController()
+    {
+
+    }
+
+    [HttpPost]
+    public IEnumerable<int> Sort(int[] array)
+    {
+        var result = BubbleSort(array);
+
+        return result;
+    }
+
+
+    private int[] BubbleSort(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = 0; j < array.Length - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    int temp = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+
+        return array;
+    }
+}
